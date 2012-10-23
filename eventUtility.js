d12be8a716a985655084c4160c2230b257132667
Travis Mullen <travis@boombangmedia.com>
2012-10-23 11:26:03 -0400

    added some feature detection

--- eventUtility.js
+++ eventUtility.js
@@ -1,5 +1,6 @@
-//cross browser events 
 var eventUtility = {
+	var doc = document;
+	//cross browser events 
     addEvent : function(el, type, fn) {
         if (typeof addEventListener !== "undefined") {
             el.addEventListener(type, fn, false);
@@ -33,6 +34,30 @@ var eventUtility = {
             event.preventDefault();
         } else {
             event.returnValue = false;
+        }
+    },
+    //element attribute detection
+    testSupport : function(el, type, attr) {
+    	var i = doc.createElement(el);
+        i.setAttribute(type, attr);
+        return i.type === attr;
+    },
+    //element detection
+    canvasSupport : function() {
+        return doc.createElement('canvas').getContext;
+    },
+    videoSupport : function() {
+        return doc.createElement('video').canPlayType;
+    },
+    //feature detection
+    geoLocSupport : function() {
+        return navigator.geolocation;
+    },
+    storageSupport : function() {
+        try {
+        	return 'localStorage' in window && window.localStorage !== null;        	
+        } catch (e) {
+        	return false;
         }
     }
 };
